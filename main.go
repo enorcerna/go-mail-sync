8968293c4742bfef9740853a1442116e9688e124
Enor Cerna <u17200479@utp.edu.pe>
2024-11-01 22:24:00 -0500

    + logger middleware

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	app := echo.New()
+	app.Use(middleware.Logger())
 	app.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "main Go app")
 	})
